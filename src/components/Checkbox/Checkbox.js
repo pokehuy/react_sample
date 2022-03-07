390b1a1a82065195ca18a36205ee41730ad8a404
Huy <pokehuy11@gmail.com>
2022-03-07 10:13:11 +0700

    initial commit

---
+++ src/components/Checkbox/Checkbox.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const Checkbox = () => {
+    
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Checkbox;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -15,3 +15,9 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+ */
+
+import App from "./App";
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<App />,document.getElementById('root'));
